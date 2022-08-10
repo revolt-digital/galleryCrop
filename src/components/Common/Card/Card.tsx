ef7d5d118454b7edd68128a86ef3be4598f40535
eapuulesrevolt <enzo.politi@revolt.digital>
2022-08-10 19:45:57 -0300

    header deleted

--- src/components/Common/Card/Card.tsx
+++ src/components/Common/Card/Card.tsx
@@ -1,7 +1,5 @@
 import React, { FC } from "react";
 import { ICard } from "../../../interfaces/types.ts";
-import { Button } from "../Button/Button";
-import CloseIcon from "@mui/icons-material/Close";
 
 export const Card: FC<ICard> = ({
   title,
@@ -12,19 +10,13 @@ export const Card: FC<ICard> = ({
   close,
 }) => {
   return (
-    <div className='cContainer'>
-      <div className='cGalleryWrapper'>
-        <div className='cHeader'>
-          <h3 className='cTitle'>{title}</h3>
-          {close && <Button onlyIcon icon={<CloseIcon />} onClick={close} />}
-        </div>
-        <div className='cBody'>
-          <h4 className='cSubtitle'>{subtitle}</h4>
-          <div className='cGalleryList'>{body}</div>
-        </div>
-        <div>{preBottom}</div>
-        <div className='cButtonsContainer'>{bottom}</div>
+    <div className="cGalleryWrapper">
+      <div className="cBody">
+        <h4 className="cSubtitle">{subtitle}</h4>
+        <div className="cGalleryList">{body}</div>
       </div>
+      <div>{preBottom}</div>
+      <div className="cButtonsContainer">{bottom}</div>
     </div>
   );
 };

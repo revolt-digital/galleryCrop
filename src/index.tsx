5fd25afb3e79d3fc1ac585c5d5e9af526106161c
Federico Correa <federicocorrea@MacBook-Pro-de-Federico-2.local>
2022-09-17 21:31:30 -0300

    changes

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,58 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import Component from "./component";
+import React, { FC } from "react";
+import CropImage from "./components/CropImage/CropImage";
+import Gallery from "./components/Gallery/Gallery";
+import useGallery from "./hooks/useGallery";
+import "./styles.css";
+import { ImageType } from "./types";
+export interface Props {
+  deckid: string;
+  aspectRatio: number;
+  onSelect: (image: ImageType) => void;
+}
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
+const GalleryCrop: FC<Props> = ({ deckid, aspectRatio, onSelect }) => {
+  const {
+    images,
+    uploadImage,
+    uploadImageLoading,
+    deleteImage,
+    currentImage,
+    isEdition,
+    setIsEdition,
+    setEditImage,
+    getImages,
+    imageSelected,
+    setImageSelected,
+  } = useGallery({ deckid });
 
-root.render(
-  // <React.StrictMode>
-  <Component />
-  // </React.StrictMode>
-);
+  if (isEdition) {
+    return (
+      <CropImage
+        deckid={deckid}
+        uploadImage={uploadImage}
+        uploadImageLoading={uploadImageLoading}
+        url={currentImage?.url!}
+        name={currentImage?.fileName!}
+        aspectRatio={aspectRatio}
+        setIsEdition={setIsEdition}
+      />
+    );
+  }
+
+  return (
+    <Gallery
+      images={images}
+      uploadImage={uploadImage}
+      uploadImageLoading={uploadImageLoading}
+      deleteImage={deleteImage}
+      setEditImage={setEditImage}
+      setIsEdition={setIsEdition}
+      getImages={getImages}
+      imageSelected={imageSelected}
+      setImageSelected={setImageSelected}
+      onSelect={onSelect}
+    />
+  );
+};
+
+export default GalleryCrop;
